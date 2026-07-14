02ad3865bab2bf3bd7781fd4424b599d937ebac7
hiinnd <hiinnd.ali03@gmail.com>
2026-07-15 02:29:03 +0300

    ااخخر

--- index.php
+++ index.php
@@ -328,7 +328,7 @@ if (!empty($_SESSION['show_success_modal'])) {
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700;800&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.0/css/all.min.css">
-    <link rel="stylesheet" href="assets/style.css?v=32">
+    <link rel="stylesheet" href="assets/style.css?v=34">
 </head>
 <body>
     
